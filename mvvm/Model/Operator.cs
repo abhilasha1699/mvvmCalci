6dc3fa6c0b9e2fca75be3802abf6beec07a9e2ea
abhilasha1699 <77963939+abhilasha1699@users.noreply.github.com>
2021-03-04 02:50:42 +0530

    Add files via upload

---
+++ mvvm/Model/Operator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mvvm.Model
+{
+    class Operator
+    {
+        public static double Calculate(double firstNumber, double secondNumber, string Operator)
+        {
+            double result = 0;
+            switch (Operator)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+
+                case "×":
+                    result = firstNumber * secondNumber;
+                    break;
+
+                case "÷":
+                    result = firstNumber / secondNumber;
+                    break;
+
+                case "%":
+                    result = firstNumber / 100;
+                    break;
+            }
+            return result;
+        }
+    }
+}
